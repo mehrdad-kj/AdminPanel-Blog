0489c9a5508f818ed183e665bd2cbbe51f4b7d89
mehrdad-kj <mehrdadk.jourabi@gmail.com>
2023-07-15 23:28:01 +0330

    initializing project

---
+++ src/components/blog/blog.component.tsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom"
+
+const Blog = () => {
+
+    const {blogId} = useParams()
+
+  return (
+    <div>Blog {blogId}</div>
+  )
+}
+
+export default Blog
---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/notfound.tsx";
+import RootLayout from "./layout/root-layout.tsx";
+import BlogsList from "./components/blogs-list/blogs.screen.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/blogs",
+        element: <BlogsList />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
